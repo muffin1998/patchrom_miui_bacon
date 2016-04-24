cc05ec57bbc9183edbce4518ab7ada087ec8543b
Just4fan <530448960@qq.com>
2016-04-24 20:45:01 +0800

    add customize_target_files.sh

---
+++ customize_target_files.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+
+cp -f other/file_contexts out/target_files/META/
+cp -r other/emmc_appsboot.mbn out/target_files/RADIO/
+cp -r other/logo.bin out/target_files/RADIO/
+cp -r other/NON-HLOS.bin out/target_files/RADIO/
+cp -r other/sdi.mbn out/target_files/RADIO/
+cp -r other/rpm.mbn out/target_files/RADIO/
+cp -r other/sbl1.mbn out/target_files/RADIO/
+cp -r other/static_nvbk.bin out/target_files/RADIO/
+cp -r other/tz.mbn out/target_files/RADIO/
