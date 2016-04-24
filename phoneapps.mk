8acd26f3a72b92f576c53a2c59197965efe1b3aa
Just4fan <530448960@qq.com>
2016-04-24 20:28:17 +0800

    use phoneapps.mk instead of "local-per-zip-misc" in makefile

---
+++ phoneapps.mk
@@ -0,0 +1,48 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := BasicDreams \
+		Bluetooth \
+		BluetoothExt \
+		CameraNext \
+		CertInstaller \
+		ConfigPanel \
+		DocumentsUI \
+		Galaxy4 \
+		HoloSpiralWallpaper \
+		HTMLViewer \
+		KeyChain \
+		LatinIME \
+		LiveWallpapers \
+		MagicSmokeWallpapers \
+		NfcNci \
+		PacProcessor \
+		PhaseBeam \
+		PhotoPhase \
+		PhotoTable \
+		PrintSpooler \
+		qcrilmsgtunnel \
+		QuickBoot \
+		shutdownlistener \
+		Stk \
+		TimeService \
+		UserDictionaryProvider \
+		VisualizationWallpapers \
+		WAPPushManager \
+
+private-phone-priv-apps := BackupRestoreConfirmation \
+		com.qualcomm.location \
+		DefaultContainerService \
+		DownloadProvider \
+		InputDevices \
+		ExternalStorageProvider \
+		MediaProvider \
+		FusedLocation \
+		ProxyHandler \
+		SettingsProvider \
+		SharedStorageBackup \
+		Shell \
+		Tag \
+		ThemesProvider \
+		VpnDialogs \
+		WallpaperCropper \
